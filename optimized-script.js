f9ff5d621c7cd1d0278f0d689eb238be1a2bb93f
Ren <developer.ren28@gmail.com>
2023-06-18 04:31:34 +0800

    Change getElementbyID object methods to object properties

--- optimized-script.js
+++ optimized-script.js
@@ -13,14 +13,14 @@ const color = {
     clearBtn: document.getElementById('clear'),
 
     getInputValue: e => color.currentValue = e.currentTarget.value,
-    applyBgColor: () => color.currentValue = grid.wrapper().style.backgroundColor,
+    applyBgColor: () => color.currentValue = grid.wrapper.style.backgroundColor,
     applyRainbow: e => e.target.style.backgroundColor = `rgb(${rng.number()}, ${rng.number()}, ${rng.number()})`,
 };
 
 const grid = {
-    wrapper: () => document.getElementById('wrapper'),
+    wrapper: document.getElementById('wrapper'),
     wrapperWidth: () => wrapper.offsetWidth,
-    items: () => gridArr = [...grid.wrapper().children],
+    items: () => gridArr = [...grid.wrapper.children],
     item: () => document.createElement('div'),
     newGrid: (gridSize) => {
             wrapper.style.display = 'flex';
@@ -58,16 +58,16 @@ const paint = {
 };
 
 const slider = {
-    range: () => document.getElementById('myRange'),
-    rangeOutput: () => document.getElementById('size-p'),
+    range: document.getElementById('myRange'),
+    rangeOutput: document.getElementById('size-p'),
     showRange: () => {
-        slider.rangeOutput().textContent = `${slider.range().value} x ${slider.range().value}`
+        slider.rangeOutput.textContent = `${slider.range.value} x ${slider.range.value}`
     }
 }
 
 const footer = {
-    selectBtn: () => document.getElementById('select'),
-    display: () => document.getElementById('footer-display'),
+    selectBtn: document.getElementById('select'),
+    display: document.getElementById('footer-display'),
     toggleDisplay: (object) => {
         object.classList.toggle('hide-display');
     }
@@ -75,9 +75,9 @@ const footer = {
 
 let menu = {
     bgStg: {
-        leftBtn: () => document.getElementById('bg-clr--lft-btn'),
-        rightBtn: () => document.getElementById('bg-clr--rght-btn'),
-        selBG: () => document.getElementById('bg-clr-label'),
+        leftBtn: document.getElementById('bg-clr--lft-btn'),
+        rightBtn: document.getElementById('bg-clr--rght-btn'),
+        selBG: document.getElementById('bg-clr-label'),
         index: 0,
         color: {
             Default: '#a4c266',
@@ -88,52 +88,52 @@ let menu = {
             const colorKeys = Object.keys(this.color);
 
             if(op === 'left') {
-                this.rightBtn().style.visibility = '';
+                this.rightBtn.style.visibility = '';
                 this.index--;
                 this.storedStg = colorKeys[this.index];
             };  
               
             if(op === 'right') {
-                this.leftBtn().style.visibility = '';
+                this.leftBtn.style.visibility = '';
                 this.index++;
                 this.storedStg = colorKeys[this.index];
             };
 
             if(this.index < 1) {
-                this.leftBtn().style.visibility = 'hidden';
+                this.leftBtn.style.visibility = 'hidden';
             } else if(this.index === colorKeys.length-1) {
-                this.rightBtn().style.visibility = 'hidden';
+                this.rightBtn.style.visibility = 'hidden';
             };
         },
         setBg: function(setting = 'Default') {
             this.selBG().textContent = setting;
-            grid.wrapper().style.backgroundColor = this.color[setting];              
+            grid.wrapper.style.backgroundColor = this.color[setting];              
         }            
     },
     gridStg: {
-        leftBtn: () => document.getElementById('grid--lft-btn'),
-        rightBtn: () => document.getElementById('grid--rght-btn'),
-        optLabel: () => document.getElementById('grid-show-label'),
+        leftBtn: document.getElementById('grid--lft-btn'),
+        rightBtn: document.getElementById('grid--rght-btn'),
+        optLabel: document.getElementById('grid-show-label'),
         setGrid: function(op) {
-            this.optLabel().textContent = 'Yes';
+            this.optLabel.textContent = 'Yes';
             grid.items().forEach((item)=>item.style.border = 'solid 1px #aaaaaa3d');
             
-            this.leftBtn().style.visibility = 'hidden';
-            this.rightBtn().style.visibility = '';
+            this.leftBtn.style.visibility = 'hidden';
+            this.rightBtn.style.visibility = 'visible';
 
             if(op === 'No') {
-                this.optLabel().textContent = op;
+                this.optLabel.textContent = op;
                 grid.items().forEach((item)=>item.style.border = 'none')    
 
-                this.leftBtn().style.visibility = '';
-                this.rightBtn().style.visibility = 'hidden';
+                this.leftBtn.style.visibility = '';
+                this.rightBtn.style.visibility = 'hidden';
             };
         }         
     }
 };
 
 function mainFunction() {
-    grid.newGrid(slider.range().value);
+    grid.newGrid(slider.range.value);
     paint.applyClick();
     slider.showRange();
     menu.bgStg.chooseStg();
@@ -159,32 +159,34 @@ function eventListeners() {
     });
     color.rainbowBtn.addEventListener('click', paint.applyHover);
     color.clearBtn.addEventListener('click', () => {
-        grid.newGrid(slider.range().value);
+        grid.newGrid(slider.range.value);
         menu.gridStg.setGrid(menu.gridStg.storedStg);
     });
 
-    slider.range().oninput = () => {
+    slider.range.oninput = () => {
         slider.showRange();
     };
-    slider.range().onchange = () => {
-        grid.newGrid(slider.range().value)
+    slider.range.onchange = () => {
+        grid.newGrid(slider.range.value)
         paint.applyClick();
         menu.gridStg.setGrid(menu.gridStg.storedStg);
     };
 
-    footer.selectBtn().addEventListener('click', () => footer.toggleDisplay(footer.display()));
+    footer.selectBtn.addEventListener('click', () => footer.toggleDisplay(footer.display()));
 
-    menu.bgStg.leftBtn().addEventListener('click', () => {
+    menu.bgStg.leftBtn.addEventListener('click', () => {
         menu.bgStg.chooseStg('left');
         menu.bgStg.setBg(menu.bgStg.storedStg)});
-    menu.bgStg.rightBtn().addEventListener('click', () => {
+        
+    menu.bgStg.rightBtn.addEventListener('click', () => {
         menu.bgStg.chooseStg('right');
         menu.bgStg.setBg(menu.bgStg.storedStg)});    
-    menu.gridStg.leftBtn().addEventListener('click', () => {
+
+    menu.gridStg.leftBtn.addEventListener('click', () => {
         menu.gridStg.storedStg = 'Yes';
         menu.gridStg.setGrid(menu.gridStg.storedStg);
     });
-    menu.gridStg.rightBtn().addEventListener('click', () => {
+    menu.gridStg.rightBtn.addEventListener('click', () => {
         menu.gridStg.storedStg = 'No';
         menu.gridStg.setGrid(menu.gridStg.storedStg);
     });    
